2ebbb1910e011d4cfc3ed883361de7486b249054
Danny Semakov <semakovdaniel@gmail.com>
2025-06-18 00:36:08 -0400

    feat: create barebones mood-energy chart
    The chart has x and y values that range from -0.5 to 0.5. Not interactive. The axes still need to be labeled and the CSS code needs to be moved to a different file.

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import MoodEnergyChart from './components/MoodEnergyChart';
+
+const sampleData = [
+  { mood: 8, energy: 7 },
+  { mood: 5, energy: 6 },
+  { mood: 3, energy: 4 },
+  { mood: 9, energy: 8 },
+];
 
 function App() {
   return (
     <div>
-      <h1>Welcome to the SoundTrail App!</h1>
+      <h1>Mood vs Energy Chart</h1>
+      <MoodEnergyChart />
     </div>
   );
 }
