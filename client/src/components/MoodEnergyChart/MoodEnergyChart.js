6ee75c28f769ca40c386ab9ce669e840160d6ac0
Danny Semakov <semakovdaniel@gmail.com>
2025-06-22 14:59:38 -0400

    feat: allow user to place a dot on any integer coordinate in the grid

--- client/src/components/MoodEnergyChart/MoodEnergyChart.js
+++ client/src/components/MoodEnergyChart/MoodEnergyChart.js
@@ -15,15 +15,48 @@ import {
   ReferenceDot
 } from "recharts";
 
+// const ImageDot = ({ cx, cy }) => {
+//   const size = 24; // size in pixels
+//   return (
+//     <image
+//       x={cx - size / 2}
+//       y={cy - size / 2}
+//       width={size}
+//       height={size}
+//       href="/your-image-file.png"
+//     />
+//   );
+// };
+
 export default function MoodEnergyChart() {
   const ticks = [-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5];
 
-  const allDataPoints = [];
-  for (let x = -5; x <= 5; x++) {
-    for (let y = -5; y <= 5; y++) {
-      allDataPoints.push({ x, y });
-    }
+  // const allDataPoints = [];
+  // for (let x = -5; x <= 5; x++) {
+  //   for (let y = -5; y <= 5; y++) {
+  //     allDataPoints.push({ x, y });
+  //   }
+  // }
+
+  const [clickPoint, setClickPoint] = useState(null);
+
+  const handleClick = (e) => {
+  const chart = chartRef.current;
+  if (!chart) return;
+
+  const { left, top, width, height } = chart.getBoundingClientRect();
+  const mouseX = e.clientX - left;
+  const mouseY = e.clientY - top;
+
+  const x = Math.round(((mouseX / width) * 10) - 5);
+  const y = Math.round(5 - (mouseY / height) * 10);
+
+  if (x >= -5 && x <= 5 && y >= -5 && y <= 5) {
+    setClickPoint({ x, y });
+  } else {
+    setClickPoint(null);
   }
+};
 
   //hoverPoint stores the integer coordinate that is closest to where the user
   //is hovering their mouse over the graph (e.g. {x: 3, y: 2}). Coordinates range
@@ -49,15 +82,34 @@ export default function MoodEnergyChart() {
     }
   };
 
+  // const [clickPoint, setClickPoint] = useState(null);
+
+  // const handleClick = (e) => {
+  // const { left, top, width, height } = chartRef.current.getBoundingClientRect();
+  // const mouseX = e.clientX - left;
+  // const mouseY = e.clientY - top;
+
+  // const x = Math.round(((mouseX / width) * 10) - 5);
+  // const y = Math.round(5 - (mouseY / height) * 10);
+  
+  //   if (x >= -5 && x <= 5 && y >= -5 && y <= 5) {
+  //     setClickPoint({ x, y });
+  //   } else {
+  //     setClickPoint(null);
+  //   }
+  // }
+
   return (
     <div
       ref={chartRef}
       onMouseMove={handleMouseMove}
       style={{ width: '60vmin', height: '60vmin', border: "2px dashed red" }}
-
+      onClick={handleClick}
     >
       <ResponsiveContainer width="100%" height="100%">
-        <ScatterChart margin={{ top: 10, right: 10, bottom: 10, left: 10 }}>
+        <ScatterChart 
+          margin={{ top: 10, right: 10, bottom: 10, left: 10 }}
+        >
           <CartesianGrid />
           <XAxis
             type="number"
@@ -97,6 +149,19 @@ export default function MoodEnergyChart() {
           <Tooltip cursor={{ strokeDasharray: "3 3" }} />
           <Scatter data={[]} 
           fill="transparent" />
+
+          {clickPoint && (
+            <ReferenceDot
+              x={clickPoint.x}
+              y={clickPoint.y}
+              r={6} // radius of the dot in pixels
+              fill="orange" // fill color of the circle
+              stroke="orange" // optional outline color
+              strokeWidth={2} // thickness of the outline
+              isFront={true} // makes sure the dot is drawn above gridlines
+            />
+          )}
+
         </ScatterChart>
       </ResponsiveContainer>
 
