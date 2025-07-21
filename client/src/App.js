a481641e5e6d3de99de063070f763e0521c15212
Danny Semakov <semakovdaniel@gmail.com>
2025-07-20 20:47:17 -0400

    fix: move mood state to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,23 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import ExplorePage from "./pages/ExplorePage";
+import { useState } from 'react'
 
 export default function App() {
+    const [mood, setMood] = useState({valence: 0.5, energy: 0.5});
+  
+
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/explore" element={<ExplorePage />} />
+        <Route
+          path="/"
+          element={<LandingPage mood={mood} setMood={setMood} />}
+        />
+        <Route
+          path="/explore"
+          element={<ExplorePage mood={mood} setMood={setMood} />}
+        />
       </Routes>
     </Router>
   );
