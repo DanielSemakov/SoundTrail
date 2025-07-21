be1836f05854d52e135afaffa8a71d4d70499dc5
Danny Semakov <semakovdaniel@gmail.com>
2025-07-20 22:14:57 -0400

    feat: allow user to update valence and energy with arrow keys and move genre state to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -5,18 +5,18 @@ import { useState } from 'react'
 
 export default function App() {
     const [mood, setMood] = useState({valence: 0.5, energy: 0.5});
-  
+    const [genre, setGenre] = useState('83dc71c7-b9da-466b-a198-bb3c29ee8f00'); 
 
   return (
     <Router>
       <Routes>
         <Route
           path="/"
-          element={<LandingPage mood={mood} setMood={setMood} />}
+          element={<LandingPage mood={mood} setMood={setMood} genre={genre} setGenre={setGenre}/>}
         />
         <Route
           path="/explore"
-          element={<ExplorePage mood={mood} setMood={setMood} />}
+          element={<ExplorePage mood={mood} setMood={setMood} genre={genre} setGenre={setGenre}/>}
         />
       </Routes>
     </Router>
--- client/src/pages/ExplorePage.js
+++ client/src/pages/ExplorePage.js
@@ -5,24 +5,95 @@ import { GetRecommendations } from '../fetch/get-recs';
 import { useNavigate } from "react-router-dom";
 import { useEffect } from 'react';
 
-export default function ExplorePage() {
+export default function ExplorePage({ mood, setMood, genre, setGenre }) {
     const navigate = useNavigate();
+
+    //test
+    useEffect(() => {
+        console.log(mood)
+    }, [mood])
 
     useEffect(() => {
         const handleKeyDown = (event) => {
         switch (event.key) {
-            case 'ArrowLeft':
+            case 'ArrowLeft': {
                 console.log('Left arrow pressed');
+
+                setMood(prev => {
+                    let newValence = Math.max(0, prev.valence - 0.1)
+                    newValence = Math.round(newValence * 10) / 10;
+
+                    return {
+                        ...prev,
+                        valence: newValence
+                    }
+                });
                 break;
-            case 'ArrowRight':
+            }
+            case 'ArrowRight': {
                 console.log('Right arrow pressed');
+
+                setMood(prev => {
+                    let newValence = Math.min(1, prev.valence + 0.1)
+                    newValence = Math.round(newValence * 10) / 10;
+
+                    return {
+                        ...prev,
+                        valence: newValence
+                    }
+                });
                 break;
-            case 'ArrowUp':
+
+                // let valence = mood.valence;
+                // valence += 0.1;
+                
+                // if (valence > 1) {
+                //     valence = 1;
+                // }
+
+                // valence = Math.round(valence * 10) / 10;
+
+                // setMood({valence: valence, energy: mood.energy});
+                // break;
+            }
+            case 'ArrowUp': {
                 console.log('Up arrow pressed');
+
+                setMood(prev => {
+                    let newEnergy = Math.min(1, prev.energy + 0.1)
+                    newEnergy = Math.round(newEnergy * 10) / 10;
+
+                    return {
+                        ...prev,
+                        energy: newEnergy
+                    }
+                });
                 break;
-            case 'ArrowDown':
+            }
+            case 'ArrowDown': {
                 console.log('Down arrow pressed');
+
+                setMood(prev => {
+                    let newEnergy = Math.max(0, prev.energy - 0.1)
+                    newEnergy = Math.round(newEnergy * 10) / 10;
+
+                    return {
+                        ...prev,
+                        energy: newEnergy
+                    }
+                });
                 break;
+
+                // let energy = mood.energy;
+                // energy -= 0.1;
+                
+                // if (energy < 0) {
+                //     energy = 0;
+                // }
+
+                // setMood({valence: mood.valence, energy: energy});
+                // break;
+            }
             default:
                 return; //ignore other keys
         }
@@ -35,7 +106,7 @@ export default function ExplorePage() {
         return () => {
         window.removeEventListener('keydown', handleKeyDown);
         };
-    }, []); //empty array: run once on mount, cleanup on unmount
+    }, [mood]); //empty array: run once on mount, cleanup on unmount
 
 
     return (
