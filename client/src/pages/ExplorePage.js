f16fa195cf0d8c0cee0cc8d2487acf22ab28daf0
Danny Semakov <semakovdaniel@gmail.com>
2025-07-22 19:13:02 -0400

    feat: add the arrow key feature to change mood back into the explore page

--- client/src/pages/ExplorePage.js
+++ client/src/pages/ExplorePage.js
@@ -5,46 +5,47 @@ import MoodEnergyChart from '../components/MoodEnergyChart/MoodEnergyChart';
 import { GetRecommendations } from '../fetch/get-recs';
 import { useLocation, useNavigate } from 'react-router-dom';
 import './ExplorePage.css';
+import useMoodKeyControls from '../hooks/useMoodKeyControls';
 
-
-export default function ExplorePage() {
+export default function ExplorePage({mood, setMood, genre, setGenre}) {
   const navigate = useNavigate();
   const location = useLocation();
+  useMoodKeyControls(mood, setMood);
 
-  const [mood, setMood] = useState(location.state?.mood || { valence: 0.5, energy: 0.5 });
+//   const [mood, setMood] = useState(location.state?.mood || { valence: 0.5, energy: 0.5 });
   const [track, setTrack] = useState(location.state?.track || null);
-  const [genre] = useState('83dc71c7-b9da-466b-a198-bb3c29ee8f00');
+//   const [genre] = useState('83dc71c7-b9da-466b-a198-bb3c29ee8f00');
 
 
-  const adjustMood = (direction) => {
-    setMood(prev => {
-      let valence = prev.valence;
-      let energy = prev.energy;
+    const adjustMood = (direction) => {
+        setMood(prev => {
+        let valence = prev.valence;
+        let energy = prev.energy;
 
-      switch (direction) {
-        case 'up': energy = Math.min(1, energy + 0.1); break;
-        case 'down': energy = Math.max(0, energy - 0.1); break;
-        case 'left': valence = Math.max(0, valence - 0.1); break;
-        case 'right': valence = Math.min(1, valence + 0.1); break;
-        default: break;
-      }
+        switch (direction) {
+            case 'up': energy = Math.min(1, energy + 0.1); break;
+            case 'down': energy = Math.max(0, energy - 0.1); break;
+            case 'left': valence = Math.max(0, valence - 0.1); break;
+            case 'right': valence = Math.min(1, valence + 0.1); break;
+            default: break;
+        }
 
-      return {
-        valence: Math.round(valence * 10) / 10,
-        energy: Math.round(energy * 10) / 10,
-      };
-    });
-  };
+        return {
+            valence: Math.round(valence * 10) / 10,
+            energy: Math.round(energy * 10) / 10,
+        };
+        });
+    };
 
-  useEffect(() => {
-    if (!mood) return;
+    useEffect(() => {
+        console.log(mood);
+    }, [mood]);
 
-    GetRecommendations(1, genre, mood).then(res => {
-      if (res?.content?.length) {
-        setTrack(res.content[0]);
-      }
-    });
-  }, [mood]);
+    GetRecommendations(1, genre, mood).then((res) => {
+        if (res?.content?.length) {
+            setTrack(res.content[0]);
+        }
+    }, [mood]);
 
   return (
     <div className="explore-page">
