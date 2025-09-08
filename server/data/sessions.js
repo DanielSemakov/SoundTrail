1c895d8dae34845e404da25d6c073f1e62691399
Danny Semakov <semakovdaniel@gmail.com>
2025-09-08 17:22:57 -0400

    feat: add playlist-user sessions array

---
+++ server/data/sessions.js
@@ -0,0 +1,6 @@
+const sessions = [
+  { sessionId: none, playlistId: '6YZJg3mWIj8SfIvbDhYfz4' },
+  { sessionId: none, playlistId: '3XM74w8aww45BxGUSM0MhP' },
+  { sessionId: none, playlistId: '6MkHxqOIx13yrmotpYmthh' },
+  { sessionId: none, playlistId: '7nPjhGrUIEEA7vznopg94L' },
+];
