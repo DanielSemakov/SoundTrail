42305ceb0e19f644466e101be0254d2d3974b2a5
Danny Semakov <semakovdaniel@gmail.com>
2026-04-11 07:41:13 -0400

    remove unused code

--- client/src/components/MoodEnergyChart/MoodEnergyChart.js
+++ client/src/components/MoodEnergyChart/MoodEnergyChart.js
@@ -59,48 +59,23 @@ export default function MoodEnergyChart({ updateMood, mood }) {
     updateMood({ valence, energy });
   };
 
-  const handleMouseMove = (e) => {
-    // kept in case you want hover behavior later
-  };
-
   return (
     <MoodEnergyChartWrapper>
       <div
         ref={chartRef}
         onClick={handleClick}
-        onMouseMove={handleMouseMove}
-        className={`${styles["chart-background"]} ${backgroundColorClass || ''}`}        
-        style={{ width: "100%", height: "100%", position: "relative", cursor: "pointer", borderRadius: 0 }}
+        className={`${styles["chart-background"]} ${backgroundColorClass || ''}`}
       >
-  
-        {/* Current mood dot */}
         {mood.valence != null && mood.energy != null && (
-          <div style={{
-            position: "absolute",
+          <div className={styles.dot} style={{
             left: `${mood.valence * 100}%`,
             top: `${(1 - mood.energy) * 100}%`,
-            width: "18px",
-            height: "18px",
-            borderRadius: "50%",
-            background: "black",
-            transform: "translate(-50%, -50%)",
-            pointerEvents: "none",
           }} />
         )}
-
-        {/* Labels */}
-        <div style={{ position: "absolute", top: "-30px", left: "50%", transform: "translateX(-50%)", fontWeight: "bold" }}>
-          Energetic
-        </div>
-        <div style={{ position: "absolute", bottom: "-30px", left: "50%", transform: "translateX(-50%)", fontWeight: "bold" }}>
-          Calm
-        </div>
-        <div style={{ position: "absolute", top: "50%", left: "-43px", transform: "translateY(-50%)", fontWeight: "bold" }}>
-          Sad
-        </div>
-        <div style={{ position: "absolute", top: "50%", right: "-63px", transform: "translateY(-50%)", fontWeight: "bold" }}>
-          Happy
-        </div>
+        <div className={styles["label-top"]}>Energetic</div>
+        <div className={styles["label-bottom"]}>Calm</div>
+        <div className={styles["label-left"]}>Sad</div>
+        <div className={styles["label-right"]}>Happy</div>
       </div>
     </MoodEnergyChartWrapper>
   );
